c41704001b409be06ea785538c03ebf5b27437a9
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-11-18 14:15:38 +0000

    add list controller new

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2024_11_18_102211) do
 
   create_table "lists", force: :cascade do |t|
-    t.string "name"
     t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
